c55a831bc83de8f16a97e3dce806857afc386ca0
Andrey Panov <andrey.panov@blacklane.com>
2017-04-23 16:19:13 +0200

    Extract correct date

--- MoviesApp/Features/MovieList/MovieViewModel.swift
+++ MoviesApp/Features/MovieList/MovieViewModel.swift
@@ -4,7 +4,7 @@ import Alamofire
 class MovieViewModel {
   
   var title: String {
-    if let date = movie.releaseDate, !date.isEmpty {
+    if let date = date {
       return movie.title + ", " + "(" + date + ")"
     }
     return movie.title
@@ -12,8 +12,15 @@ class MovieViewModel {
   var overview: String {
     return movie.overview ?? "not set"
   }
-  var date: String {
-    return movie.releaseDate ?? "unowned"
+  var date: String? {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd"
+    guard let date = formatter.date(from: movie.releaseDate ?? "") else { return nil }
+    let components = Calendar.current.dateComponents([.year], from: date)
+    if let year = components.year {
+      return "\(year)"
+    }
+    return nil
   }
   var image: UIImage?
   
@@ -30,10 +37,10 @@ class MovieViewModel {
     let request = Alamofire.download("http://image.tmdb.org/t/p/w92" + path)
     request.responseData { [weak self] response in
       switch response.result {
-      case .success(let data):
-        self?.image = UIImage(data: data)
-        onSuccess(self?.image)
-      default:()
+        case .success(let data):
+          self?.image = UIImage(data: data)
+          onSuccess(self?.image)
+        default:()
       }
     }
   }
